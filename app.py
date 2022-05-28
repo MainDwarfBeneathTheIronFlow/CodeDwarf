4f73f702bf5779447fd5046ffc3e73f606dd0b00
MainDwarfBeneathTheIronFlow <105614913+MainDwarfBeneathTheIronFlow@users.noreply.github.com>
2022-05-28 17:18:04 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {42}
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int):
+#     return {"item_id": item_id}
