9e1f862de5176ed6b710f79e9945fce145743005
MainDwarfBeneathTheIronFlow <3dnobeejustweletusseeletterb@gmail.com>
2022-06-19 07:28:21 +1000

    Rebuilded DB and added login page, no post page

---
+++ sql_db/schemas.py
@@ -0,0 +1,60 @@
+from typing import List, Union
+
+from pydantic import BaseModel
+
+
+class PostBase(BaseModel):
+    title: str
+    description: Union[str, None] = None
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    img_name: str
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+
+
+class UserBase(BaseModel):
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    avatar_name: str
+    is_active: bool
+    posts: List[Post] = []
+
+    class Config:
+        orm_mode = True
+
+
+
+
+class GameBase(BaseModel):
+    title: str
+    description: Union[str, None] = None
+    system_requirements: Union[str, None] = None
+
+
+class GameCreate(GameBase):
+    pass
+
+
+class Game(GameBase):
+    id: int
+
+    class Config:
+        orm_mode = True
