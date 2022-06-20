d97c823bf8003f20f9a9640598e24be3e46edd47
MainDwarfBeneathTheIronFlow <105614913+MainDwarfBeneathTheIronFlow@users.noreply.github.com>
2022-06-20 10:48:10 +1000

    Token to Shemas

--- sql_db/schemas.py
+++ sql_db/schemas.py
@@ -2,6 +2,14 @@ from typing import List, Union
 
 from pydantic import BaseModel
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
 
 class PostBase(BaseModel):
     title: str
