4c215d69bd0fed394bc064eb6ceb6e580c606e03
MainDwarfBeneathTheIronFlow <105614913+MainDwarfBeneathTheIronFlow@users.noreply.github.com>
2022-06-27 12:47:24 +1000

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Settings:
     SECRET_KEY = "483235a3e50662ca32026396f418b332bd50278271f390a4ecd5473f1dcd9a55"
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30  # in mins
+    SUPERPASSWORD = "0987654321"
 
 
 settings = Settings()
